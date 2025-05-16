04647590a9c656c92a341d982cf4d258a9cf1e29
meeantissimo <abagoo@yahoo.com>
2025-05-16 21:54:37 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,158 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . f f f f f . . 
+. . . . . . . . f f 1 1 1 f . . 
+. . f f f f f . f 1 1 1 f 1 . . 
+. . f 1 1 1 1 f f f f f 1 f . . 
+. . f 1 1 1 f 4 4 4 4 4 f . . . 
+. . f f 1 1 1 4 4 4 4 4 f . . . 
+. . . f 4 4 4 4 4 4 4 4 f f . . 
+. . f f 4 4 4 4 4 4 f f 4 f f . 
+. f 4 f 4 4 4 4 4 f f 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 f f 4 f 
+. f f f 4 4 4 4 4 4 4 4 f f f f 
+. . . f 4 4 f f f f 4 4 f . . . 
+. . . f 4 4 f . . f 4 4 f . . . 
+. . . f f f f . . f f f f . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . . . . . f f f f f . . 
+. . . . . . . . f f 1 1 1 f . . 
+. . f f f f f . f 1 1 1 f 1 . . 
+. . f 1 1 1 1 f f f f f 1 f . . 
+. . f 1 1 1 f 4 4 4 4 4 f . . . 
+. . f f 1 1 1 4 4 4 4 4 f . . . 
+. . . f 4 4 4 4 4 4 4 4 f f . . 
+. . f f 4 4 4 4 4 4 f f 4 f f . 
+. f 4 f 4 4 4 4 4 f f 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 f f 4 f 
+. f f f 4 4 4 4 4 4 4 4 f f f f 
+. . . f 4 4 f f f f 4 4 f . . . 
+. . . f 4 4 f . . f 4 4 f . . . 
+. . . f f f f . . f f f f . . . 
+`;
+            case "1RI,DWlrJQ)Mk2spvCDd":
+            case "Randy":return img`
+. . . . . . . . . f f f f f . . 
+. . . . . . . . f f 1 1 1 f . . 
+. . f f f f f . f 1 1 1 f 1 . . 
+. . f 1 1 1 1 f f f f f 1 f . . 
+. . f 1 1 1 f 4 4 4 4 4 f . . . 
+. . f f 1 1 1 4 4 4 4 4 f . . . 
+. . . f 4 4 4 4 4 4 4 4 f f . . 
+. . f f 4 4 4 4 4 4 f f 4 f f . 
+. f 4 f 4 4 4 4 4 f f 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 f f 4 f 
+. f f f 4 4 4 4 4 4 4 4 f f f f 
+. . . f 4 4 f f f f 4 4 f . . . 
+. . . f 4 4 f . . f 4 4 f . . . 
+. . . f f f f . . f f f f . . . 
+`;
+            case "image4":
+            case "myImage":return img`
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 1 1 2 . . . . 
+. . . . 2 2 2 2 2 2 1 1 2 . . . 
+. . . 2 2 2 2 2 2 2 2 1 2 2 . . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . 2 2 2 2 2 2 2 2 1 2 2 . . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . 2 2 2 2 2 2 2 2 2 . . . 
+. . . . . 2 2 2 2 2 2 2 . . . . 
+. . . . . . 2 2 2 2 2 . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+`;
+            case "image5":
+            case "myImage2":return img`
+. . . f f f f . . f f f . . . . 
+. . f 1 1 1 1 f f 1 1 1 f . . . 
+. . f 1 f 1 1 7 7 1 f 1 f . . . 
+. . . 1 1 1 f 7 f f 1 1 f . . . 
+. . . . f f f 7 7 f 7 7 7 f . . 
+. . . f f e 7 7 7 7 7 7 7 f f . 
+. . . f e e 7 7 7 f 7 7 7 e f f 
+. . . f e e f 7 7 7 7 f 7 e f f 
+4 f f f e e 7 f 7 f 7 7 7 f f f 
+4 f f f e e e e f 7 7 e f f 7 f 
+. . 7 f e e e e e e e e f 7 f f 
+. . . f e e e f f f f f f f e f 
+. . . f e e e f f f f f e e e f 
+. . . f e e e e e e e e e e f f 
+. . . f f f 7 7 f . f 7 7 f . . 
+. . . . . f f f f . f f f f . . 
+`;
+            case "image6":
+            case "myImage3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 5 4 4 4 . . . . . . 
+. . . . . . 5 4 4 4 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "Randy jump":return img`
+. . . . . . . . . f f f f f . . 
+. . . . . . . . f f 1 1 1 f . . 
+. . f f f f f . f 1 1 1 f 1 . . 
+. . f 1 1 1 1 f f f f f 1 f . . 
+. . f 1 1 1 f 4 4 4 4 4 f . f f 
+. . f f 1 1 1 4 4 4 4 4 f f f f 
+. . . f 4 4 4 4 4 4 4 4 f f 4 f 
+. . . f 4 4 4 4 4 4 f f 4 f 4 f 
+. f f f 4 4 4 4 4 f f 4 4 f 4 f 
+. f 4 f 4 4 4 4 4 4 4 4 4 f f f 
+f f 4 f 4 4 4 4 4 4 4 4 4 f . . 
+f 4 4 f 4 4 4 4 4 4 4 4 f f . . 
+f 4 f f 4 4 4 4 4 4 4 4 f f . . 
+f f f 4 4 4 f f f 4 4 4 f . . . 
+. . f 4 4 f f . f 4 4 f f . . . 
+. . . f f f . . f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
